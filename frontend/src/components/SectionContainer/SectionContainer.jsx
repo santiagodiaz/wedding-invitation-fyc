fedccde09c3693bf4c10d56d61dea3b0e5eede5b
Santiago Diaz <santiago.d.diaz@gmail.com>
2024-12-10 09:27:59 -0300

    add spaces

--- frontend/src/components/SectionContainer/SectionContainer.jsx
+++ frontend/src/components/SectionContainer/SectionContainer.jsx
@@ -3,7 +3,16 @@ const SectionContainer = ( { children } ) => {
       <div className='w-full 
          lg:w-10/12 lg:flex lg:justify-between lg:gap-6'>
          { children }
+         <br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
       </div>
+      
    )
 }
 
