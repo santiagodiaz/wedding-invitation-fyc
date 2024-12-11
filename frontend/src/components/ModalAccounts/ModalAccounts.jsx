c82d6641aad7482eb4702520183ac7f15c97067d
Santiago Diaz <santiago.d.diaz@gmail.com>
2024-12-11 09:59:31 -0300

    update paymenent modal

--- frontend/src/components/ModalAccounts/ModalAccounts.jsx
+++ frontend/src/components/ModalAccounts/ModalAccounts.jsx
@@ -22,11 +22,11 @@ const ModalAccounts = () => {
          lg:w-[720px]">
 
          <header className="relative flex flex-col items-center justify-start gap-2 w-full rounded-t-md
-            bg-[url('/assets/backgrounds/cbu-header-vertical.png')] bg-no-repeat bg-cover bg-bottom ">
+            bg-gray-dark bg-no-repeat bg-cover bg-bottom ">
             <div className="h-[150px] md:h-[150px] lg:h-[150px] flex pt-[4vh]">
                <h2 className="text-2xl font-semibold text-center text-white z-50
                   lg:text-3xl">
-                  ¿No sabés que <br className="md:hidden"></br> regalarnos?
+                  ¿No sabés qué <br className="md:hidden"></br> regalarnos?
                </h2>
             </div> 
             <img 
@@ -36,10 +36,10 @@ const ModalAccounts = () => {
                className="absolute top-4 right-4 h-10 cursor-pointer rounded-md z-50
                   transition-all delay-50 duration-150 hover:cursor-pointer hover:scale-90 hover:drop-shadow-md hover:rotate-90" 
             />
-            <img src="./assets/images/cbu-header-pieces-03.png" alt="Gift animation" className="h-16 absolute -bottom-4 right-5 jumping-element lg:h-20 lg:-bottom-4 lg:right-12" />
+            <img src="./assets/images/cbu-header-pieces-03.png" alt="Gift animation" className="h-16 absolute -bottom-1 right-5 jumping-element lg:h-20 lg:-bottom-4 lg:right-12" />
             <img src="./assets/images/cbu-header-pieces-06.png" alt="Star animation" className="h-4 absolute top-2 left-12 jumping-element-reverse lg:h-5 lg:top-5 lg:left-20" />
             <img src="./assets/images/cbu-header-pieces-04.png" alt="Coins animation" className="h-10 absolute top-16 left-2 jumping-element-reverse lg:h-16 lg:top-16 lg:left-12" />
-            <img src="./assets/images/cbu-header-pieces-01.png" alt="" className="h-6 absolute bottom-2 left-5 jumping-element lg:h-8 lg:-bottom-2" />
+         
          </header>
 
          <main className='flex flex-col items-center gap-8 grow bg-cream p-6 text-gray-dark text-sm'>
@@ -49,22 +49,22 @@ const ModalAccounts = () => {
             lg:w-[450px]">
                <section className="flex items-center gap-2">
                   <img src="./assets/images/green-arrows-icon.png" alt="" className='h-6'/>
-                  <h2 className='font-semibold text-base'>BANCO BBVA</h2>
+                  <h2 className='font-semibold text-base'>BANCO GALICIA</h2>
                </section>
                <div className="flex flex-col gap-1.5">
                   <div className='flex-wrap'>
                      <span className='italic'>Titular :</span>
-                     <span className='font-semibold'> Federico Rouyere</span>
+                     <span className='font-semibold'> Julieta Salas</span>
                   </div>
                   <div className='flex items-center gap-2 flex-wrap'>
                      <span className='italic'>Alias :</span>
                         <div className='flex items-center gap-2'>
-                           <span className='font-semibold'> FEDERICO.CASPER.BBVA</span>
+                           <span className='font-semibold'> JULIETA.PAMELA.SALAS</span>
                         </div>
                         <button
                            className="group relative inline-flex " >
                            <img 
-                              onClick={ () => copyToClipboard('FEDERICO.CASPER.BBVA') } 
+                              onClick={ () => copyToClipboard('JULIETA.PAMELA.SALAS') } 
                               src="./assets/images/copy-icon.png" 
                               alt=" Boton copiar " 
                               title='Copiar' 
@@ -73,7 +73,7 @@ const ModalAccounts = () => {
                            <div className="hidden group-hover:block">
                               <div
                                  className="group absolute -top-12 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center rounded-sm text-center text-sm text-cream before:-top-2">
-                                 <div className={`rounded-md py-1 px-2 ${ copied ? 'bg-mustard' : 'bg-green' }`}>
+                                 <div className={`rounded-md py-1 px-2 ${ copied ? 'bg-sky' : 'bg-green' }`}>
                                     <p className="whitespace-nowrap">{ copied ? 'Copiado!' : 'Copiar' }</p>
                                  </div>
                               </div>
@@ -81,17 +81,21 @@ const ModalAccounts = () => {
                         </button>
                   </div>
                   <div className='flex-wrap'>
-                     <span className='italic'>CUIL/CUIT :</span>
-                     <span className='font-semibold'> 20308193242</span>
+                     <span className='italic'>CUIL :</span>
+                     <span className='font-semibold'> 27387005108</span>
+                  </div>
+                  <div className='flex-wrap'>
+                     <span className='italic'>CTA :</span>
+                     <span className='font-semibold'> 4040441-1 038-4</span>
                   </div>
                   <div className='flex items-center gap-2 flex-wrap'>
                      <span className='italic'>CBU :</span>
                      <div className='flex items-center gap-2'>
-                        <span className='font-semibold'> 0170237040000005581238</span>
+                        <span className='font-semibold'> 0070038230004040441143</span>
                         <button
                            className="group relative inline-flex " >
                            <img 
-                              onClick={ () => copyToClipboard('0170237040000005581238') } 
+                              onClick={ () => copyToClipboard('0070038230004040441143') } 
                               src="./assets/images/copy-icon.png" 
                               alt=" Boton copiar " 
                               title='Copiar' 
@@ -100,7 +104,7 @@ const ModalAccounts = () => {
                            <div className="hidden group-hover:block">
                               <div
                                  className="group absolute -top-12 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center rounded-sm text-center text-sm text-[#EAE8E4] before:-top-2">
-                                 <div className={`rounded-md py-1 px-2 ${ copied ? 'bg-mustard' : 'bg-green' }`}>
+                                 <div className={`rounded-md py-1 px-2 ${ copied ? 'bg-sky' : 'bg-green' }`}>
                                     <p className="whitespace-nowrap">{ copied ? 'Copiado!' : 'Copiar' }</p>
                                  </div>
                               </div>
@@ -111,19 +115,6 @@ const ModalAccounts = () => {
                </div>
             </article>
 
-            {/* Dolars Section */}
-            <article className="flex flex-col gap-2 w-full
-               lg:w-[450px]">
-               <section className="flex items-center gap-2">
-                  <img src="./assets/images/green-arrows-icon.png" alt="" className='h-6'/>
-                  <h2 className='font-semibold text-base'>MONEDA EXTRANJERA</h2>
-               </section>
-               <p >
-                  <span className='italic text-sm leading-6'>
-                     No tenemos cuentas para recibir dinero extranjero, por lo tanto 👉🏼 sobre en mano.<br></br>
-                  </span>
-               </p>
-            </article>
 
 
                <article className='flex flex-col gap-3 items-center mt-2'>
