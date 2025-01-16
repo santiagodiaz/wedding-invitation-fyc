12b59d541568a74e564621b6554a77e5b1217b0a
Santiago Diaz <santiago.d.diaz@gmail.com>
2025-01-15 21:18:47 -0300

    udpate images

--- frontend/src/components/MainHeadline/MainHeadline.jsx
+++ frontend/src/components/MainHeadline/MainHeadline.jsx
@@ -2,19 +2,9 @@ const MainHeadline = () => {
    return (
       
       <section className="z-50 flex flex-col items-center gap-5 absolute inset-0 mx-auto top-[40%] lg:top-[35%]">
-         <br></br>
-         <br></br>
-         <br></br>
-         <br></br>
-         <br></br>
-         <br></br>
-         <br></br>
-         <br></br>
-         <br></br>
          <img src="/assets/images/names.png" alt="" className="w-72 lg:w-96"/>
-         <hr className="w-44 border-1 border-green " />
          <h3 className="text-center text-white text-[16px] mt-2 drop-shadow-lg lg:text-[24px]">
-            8 de febrero 2025
+            9 de marzo 2025
          </h3>
       </section>
    )
